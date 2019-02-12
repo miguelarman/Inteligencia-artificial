11e6f04627fb65c029fc641dcb48a835e94a06bd
DCabornero <dcabornero@gmail.com>
2019-02-12 14:20:47 +0100

    Parte 2 acabada

--- Pr_2/ej3.lisp
+++
@@ -1,5 +0,0 @@
-(defun all-roots-newton (f df max-iter semillas &optional (tol 0.001))
-  (if (null semillas)
-      NIL
-    (cons (newton f df max-iter (first semillas) tol) 
-          (all-roots-newton f df max-iter (rest semillas) tol))))
