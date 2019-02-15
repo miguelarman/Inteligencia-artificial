b651fc6c4ae0cadc5faabd9cf27e7ef3566ad0b7
Miguel Arconada WIN <miguelarman@gmail.com>
2019-02-15 11:38:09 +0100

    ej4.lisp

---
+++ Pr_1/project1/ej4.lisp
@@ -0,0 +1,48 @@
+;; Ejercicio 4
+
+(defconstant +bicond+ '<=>)
+(defconstant +cond+ '=>)
+(defconstant +and+ '^)
+(defconstant +or+ 'v)
+(defconstant +not+ '!)
+
+
+(defun truth-value-p (x)
+  (or (eql x T) (eql x NIL)))
+
+(defun unary-connector-p (x)
+  (eql x +not+))
+
+(defun binary-connector-p (x)
+  (or (eql x +bicond+)
+      (eql x +cond+)))
+
+(defun n-ary-connector-p (x)
+  (or (eql x +and+)
+      (eql x +or+)))
+
+(defun bicond-connector-p (x)
+  (eql x +bicond+))
+
+(defun cond-connector-p (x)
+  (eql x +cond+))
+
+(defun connector-p (x)
+  (or (unary-connector-p x)
+      (binary-connector-p x)
+      (n-ary-connector-p x)))
+
+(defun positive-literal-p (x)
+  (and (atom x)
+       (not (truth-value-p x))
+       (not (connector-p x))))
+
+(defun negative-literal-p (x)
+  (and (listp x)
+       (eql +not+ (first x))
+       (null (rest (rest x)))
+       (positive-literal-p (second x))))
+
+(defun literal-p (x)
+  (or (positive-literal-p x)
+      (negative-literal-p x)))
